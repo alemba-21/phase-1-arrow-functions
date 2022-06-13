80c5edb9fbc45023b84056baa854dc476d693952
Alemba <alembakiragu@gmail.com>
2022-06-14 02:49:02 +0300

    commit tree d8d556b8733f1a2fa5e306d3eec1f1c338848d49

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-const add = (parameter1, parameter2) => parameter1 + parameter2;
-add(2,3);
+const add = (a,b) => (a+b);
+add(2,3)
 
-const square = (parameter1) => parameter1 * 2;
+const square = a => a * 2;
 
-const divide = () => 2000/100
+const divide = (a,b) => 2000/100
